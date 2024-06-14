5378b7afcd9e930626e40e90cc833ac2ee601fc3
Svetlana Soboleva <blankor@mail.ru>
2024-06-14 22:18:51 +0400

    Домашняя работа 3 Массивы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,23 @@
+'use strict'
+
+// Задача 1. Сравнить массивы
+
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    let result = arr1.every((value, index) => value === arr2[index]);
+    return result;
 }
 
+// Задача 2. Фильтрация и преобразование массива
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let genderFilter = users.filter(users => users.gender === gender);
+    if (genderFilter.length === 0) {
+        return 0;
+    }
+    let totalAge = genderFilter.map(genderFilter => genderFilter.age).reduce((acc, age) => acc + age, 0);
+    let result = totalAge / genderFilter.length;
+    return result;
 }
